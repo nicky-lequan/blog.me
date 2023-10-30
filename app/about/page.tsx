47623522ae849d97a92072b41cc4acdc9a9a24c0
jooncco <jooncco.g@gmail.com>
2023-10-30 22:20:05 +0900

    :pencil2: edit: hero section content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,16 +13,16 @@ export default function About(): JSX.Element {
           <h1 className="mt-2 font-black text-[40px] xs:text-[50px] sm:text-[60px] lg:text-[80px] lg:leading-[98px]">
             Hi there. I'm <span className="text-secondary">JunHa.</span>
           </h1>
-          <p className="mt-2 text-gray-700 dark:text-gray-500 font-medium text-[16px] xs:text-[20px] sm:text-[26px] lg:text-[30px] lg:leading-[40px]">
+          <p className="mt-2 text-gray-700 dark:text-gray-500 font-medium text-[16px] xs:text-[20px] sm:text-[26px] lg:text-[30px] leading-[32px]">
             I'm a full-stack software engineer. <br />
           </p>
-          <p className="mt-2 text-gray-700 dark:text-gray-500 font-medium text-[10px] xs:text-[14px] sm:text-[18px] lg:text-[24px] lg:leading-[32px]">
+          <p className="mt-2 text-gray-700 dark:text-gray-500 font-medium text-[10px] xs:text-[14px] sm:text-[18px] lg:text-[24px] leading-[24px]">
             AWS, K8s, Next.js, and Competitive Programming are the things that
             I'm into nowadays. I have more capabilities of course, so please
-            refer to the Skills section if you wish to see more. I'm currently
-            open for job opportunities by the way, especially those in Canada.
-            So please don't hesitate to reach me if you're a manager, head
-            hunter, or whatever your position is. Weight training, watching
+            refer to the Skills section if you wish to see more. <br />
+            I'm currently open to job opportunities by the way, especially those
+            in Canada. So please don't hesitate to reach me if you're a manager,
+            head hunter, or whatever your position is. Weight training, watching
             performances, going to music festivals and snow boarding are my top
             hobbies. I listen to jazzy-house mix when I work, and I'm also an
             inevitable coffee lover.
