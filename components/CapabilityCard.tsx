c7b57872b30b6dd1de5c9c9cac93ceb5030403b3
jooncco <jooncco.g@gmail.com>
2023-10-31 23:56:27 +0900

    :art: green-violet-gradient

--- components/CapabilityCard.tsx
+++ components/CapabilityCard.tsx
@@ -20,7 +20,7 @@ export default function CapabilityCard({
     <Tilt className="sm:w-[180px] w-full">
       <motion.div
         variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] drop-shadow-2xl">
+        className="w-full green-violet-gradient p-[1px] rounded-[20px] drop-shadow-2xl">
         <div className="bg-neutral rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
           <Image src={icon} alt={title} className="w-16 h-16 object-contain" />
           <h3 className="text-[20px] font-bold text-center">{title}</h3>
