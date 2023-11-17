dd8bd0984e019f2c4df1a2f7d2f558d219e61968
jooncco <jooncco.g@gmail.com>
2023-11-17 15:13:05 +0900

    :art: CapabilityCard background

--- components/CapabilityCard.jsx
+++ components/CapabilityCard.jsx
@@ -17,7 +17,7 @@ export default function CapabilityCard({index, title, icon}) {
         className="w-full green-violet-gradient p-[1px] rounded-[20px] drop-shadow-2xl">
         <div
           options={{max: 45, scale: 1, speed: 450}}
-          className="bg-neutral rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
+          className="bg-neutral2 rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
           <Image src={icon} alt={title} className="w-16 h-16 object-contain" />
           <h3 className="text-[20px] font-bold text-center">{title}</h3>
         </div>
