51c6fab8c2a9262f11a1573e5ba30e078ce2d287
jooncco <jooncco.g@gmail.com>
2024-01-12 13:00:03 +0900

    :recycle: refactor: Work section

--- components/Work.jsx
+++ components/Work.jsx
@@ -23,72 +23,71 @@ const experiences = [
     companyName: 'LG Electronics',
     iconSrc: '/img/company/lg.png',
     date: 'Feb 2023 - Nov 2023',
-    contribution: [
-      'LGE SDP AppMod project: next generation IP TV web servers.',
-      'Contribution:',
-      '- Implemented REST APIs for IP TVs, which authenticates & initializes client module based on device meta data.',
-      '- Reduced execution time of a device initialization API into half by applying async executors (@Async annotation in Spring).',
-      '- Designed & implemented a batch micro service which periodically uploads log files to S3 bucket for monitoring.',
-      'Hands on: Spring Boot, MyBatis, Gradle, MySQL, AWS, Gerrit(VCS)',
+    description: 'LGE SDP AppMod project: next generation IP TV web servers.',
+    contributions: [
+      'Implemented REST APIs for IP TVs: authenticates & initializes client module based on device meta data.',
+      'Reduced execution time of a device initialization API into half by applying async executors (@Async annotation in Spring).',
+      'Designed & implemented a batch micro service which periodically uploads log files to S3 bucket for monitoring.',
     ],
+    stack: 'Spring Boot, MyBatis, MySQL, AWS',
   },
   {
     title: 'Full-stack Engineer',
     companyName: 'Bithumb meta',
     iconSrc: '/img/company/bithumb.png',
     date: 'Mar 2022 - Dec 2022',
-    contribution: [
-      'NFT marketplace Naemo World: https://naemo.io',
-      'Contribution:',
-      '- My Wallet page & related REST APIs (connext wallet, get balance, etc.)',
-      '- SNS OAuth (Twitter, Discord, Telegram, Instagram)',
-      '- SNS Event Mission page & related REST APIs',
-      '- Ethereum/Solana withdraw REST API',
-      '- Admin Launchpad List page & related REST APIs. Launchpad is a collection of NFTs in promo.',
-      'Hands on: Spring Boot, Spring Data JPA, QueryDSL-JPA, MySQL, React.js with mobX, Gradle, AWS',
+    description: 'NFT marketplace Naemo World: https://naemo.io',
+    contributions: [
+      'My Wallet page & related REST APIs (connext wallet, get balance, etc.)',
+      'SNS OAuth (Twitter, Discord, Telegram, Instagram)',
+      'SNS Event Mission page & related REST APIs',
+      'Ethereum/Solana withdraw REST API',
+      'Admin Launchpad List page & related REST APIs. Launchpad is a collection of NFTs in promo.',
     ],
+    stack:
+      'Spring Boot, Spring Data JPA, QueryDSL-JPA, MySQL, React.js with mobX, AWS',
   },
   {
     title: 'Full-stack Engineer',
     companyName: 'LG U+',
     iconSrc: '/img/company/lg.png',
     date: 'Jul 2021 - Feb 2022',
-    contribution: [
+    description:
       'Cloud resource management platform UCMP: a platform we can manage standardized GCP/AWS cloud resources with ease.',
-      'Contribution: ',
-      '- Implemented 2FA via email',
-      '- Sign up, sign out page & related REST APIs',
-      '- Cloud resource creation page & related REST APIs',
-      '- Email notification using Amazon SNS',
-      'Hands on: Spring Boot, MyBatis, Thymeleaf, React.js, Gradle, MySQL, AWS, Github APIs',
+    contributions: [
+      'Implemented 2FA via email',
+      'Sign up, sign out page & related REST APIs',
+      'Cloud resource creation page & related REST APIs',
+      'Email notification using Amazon SNS',
     ],
+    stack: 'Spring Boot, MyBatis, Thymeleaf, React.js, MySQL, AWS, Github APIs',
   },
   {
     title: 'Full-stack Engineer',
     companyName: 'Hanwha Life Insurance',
     iconSrc: '/img/company/hanwha.png',
     date: 'Feb 2021 - Jun 2021',
-    contribution: [
+    description:
       'Hanhwa point platform P3: reliable point management based on blockchain ledger.',
-      'Contribution:',
-      '- Point transaction REST APIs (payment, transfer, refund)',
-      '- Back office: point usage history page & related REST APIs',
-      'Hands on: Spring Boot, MyBatis, Gradle, React.js, MySQL, Klaytn API, AWS',
+    contributions: [
+      'Point transaction REST APIs (payment, transfer, refund)',
+      'Back office: point usage history page & related REST APIs',
     ],
+    stack: 'Spring Boot, MyBatis, React.js, MySQL, AWS',
   },
   {
     title: 'Backend Engineer',
     companyName: 'Okimoki',
     iconSrc: '/img/company/okimoki.png',
     date: 'Jan 2020 - Dec 2020',
-    contribution: [
+    description:
       'LG CNS coffee order chatbot Okimoki: order your coffee inside Kakao talk.',
-      'Contribution:',
-      '- Designed & implemented 3 back-end microservices (store, menu, order)',
-      '- Coffee coupon data modeling',
-      '- Designed & implemented user authentication',
-      'Hands on: Spring Boot, Spring Data JPA, Spring Webflux, Gradle, Postgre SQL, AWS',
+    contributions: [
+      'Designed & implemented 3 back-end microservices (store, menu, order)',
+      'Coffee coupon data modeling',
+      'Designed & implemented user authentication',
     ],
+    stack: 'Spring Boot, Spring Data JPA, Spring Webflux, Postgre SQL, AWS',
   },
 ];
 
@@ -96,7 +95,7 @@ const Work = () => (
   <>
     <motion.div variants={textVariant()}>
       <p className="md:text-[16px] text-[12px] text-gray-700 dark:text-gray-500 uppercase tracking-wider">
-        Projects I&apos;ve experienced in LG CNS
+        Projects
       </p>
       <h1 className="text-text font-black md:text-[50px] sm:text-[42px] text-[30px]">
         Work Experience.
@@ -137,7 +136,7 @@ const Work = () => (
               <Typography
                 variant="small"
                 className="text-[12px] font-bold font-raleway">
-                {experience.companyName}
+                Client: {experience.companyName}
               </Typography>
               <Typography
                 variant="small"
@@ -145,13 +144,31 @@ const Work = () => (
                 {experience.date}
               </Typography>
               <ul>
-                {experience.contribution.map((point, index) => (
-                  <li key={index}>
+                <li>
+                  <Typography className="font-bold text-stone-600 dark:text-stone font-heavydata text-[14px]">
+                    {experience.description}
+                  </Typography>
+                </li>
+                {experience.contributions && (
+                  <li>
                     <Typography className="font-medium text-stone-600 dark:text-stone font-heavydata text-[14px]">
-                      {point}
+                      Contributions:
                     </Typography>
                   </li>
-                ))}
+                )}
+                {experience.contributions &&
+                  experience.contributions.map((contribution, index) => (
+                    <li key={index}>
+                      <Typography className="font-medium text-stone-600 dark:text-stone font-heavydata text-[14px]">
+                        - {contribution}
+                      </Typography>
+                    </li>
+                  ))}
+                <li>
+                  <Typography className="font-medium text-stone-600 dark:text-stone font-heavydata text-[14px]">
+                    Stack: {experience.stack}
+                  </Typography>
+                </li>
               </ul>
             </TimelineBody>
           </TimelineItem>
