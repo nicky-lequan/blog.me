ea33881d72b34b9225daa4f8024ab55da43ec54a
jooncco <jooncco.g@gmail.com>
2024-05-12 21:37:41 +0900

    :heavy_minus_sign: remove resume download buttons

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,10 @@
 import Contact from '@/components/Contact/Contact';
-import Resume from '@/components/Resume/Resume';
 
 function ContactPage() {
   return (
     <div className="px-0 sm:px-8 flex flex-col bg-neutral rounded-2xl drop-shadow-lg">
       <Contact />
-      <Resume />
+      {/* <Resume /> */}
     </div>
   );
 }
